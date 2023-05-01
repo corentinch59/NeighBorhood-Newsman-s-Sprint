3154993a4b3eeff92c932902f9c65581d81324dc
Corentin Chrisment <tintinch59@gmail.com>
2023-05-01 22:39:18 +0200

    reverted circleAim code

--- Assets/Scripts/CharacterAim.cs
+++ Assets/Scripts/CharacterAim.cs
@@ -36,20 +36,6 @@ public class CharacterAim : MonoBehaviour
             anim.SetBool("Throw", true);
             Time.timeScale = 0.5f;
             isAiming = true;
-
-            // Create a circle with the correct radius and alpha
-            GameObject circle = new GameObject("AimCircle");
-            circle.transform.SetParent(aimCamera.transform);
-            circle.transform.localScale = Vector3.one * 3f; // Start larger than the target size
-            SpriteRenderer sr = circle.AddComponent<SpriteRenderer>();
-            sr.sprite = Resources.Load<Sprite>("CircleSprite");
-            sr.color = new Color(1f, 1f, 1f, 0f); // Start with zero alpha
-            sr.sortingOrder = 10;
-            float targetSize = radius * 2f * 100f;
-            circle.transform.DOScale(new Vector3(targetSize, targetSize, 1f), 0.5f)
-                .SetEase(Ease.OutBack)
-                .SetUpdate(true);
-            sr.DOFade(0.5f, 0.5f); // Fade in the circle
         }
         else
         {
@@ -60,17 +46,6 @@ public class CharacterAim : MonoBehaviour
             Time.timeScale = 1;
             isAiming = false;
 
-            // Destroy the circle with an animation
-            GameObject circle = GameObject.Find("AimCircle");
-            if (circle != null)
-            {
-                SpriteRenderer sr = circle.GetComponent<SpriteRenderer>();
-                sr.DOFade(0f, 0.5f); // Fade out the circle
-                circle.transform.DOScale(Vector3.one * 3f, 0.5f) // Grow the circle before destroying
-                    .SetEase(Ease.InBack)
-                    .OnComplete(() => Destroy(circle))
-                    .SetUpdate(true);
-            }
             ThrowLetter();
         }
     }
@@ -84,7 +59,6 @@ public class CharacterAim : MonoBehaviour
 
             // Get the closest target to the player's position and set it as the current target
             TargetableObject closestTarget = targetingSystem.GetClosestTarget(Camera.main, maxDistance, radius);
-
         }
     }
 
@@ -92,4 +66,5 @@ public class CharacterAim : MonoBehaviour
     {
         Debug.Log("i threw my letter");
     }
+
 }
